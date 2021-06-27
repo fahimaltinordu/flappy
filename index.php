79dc129036c9eeb5036266ba52a27cbe5ceac298
fahimaltinordu <altinordu.fahim@gmail.com>
2021-06-27 19:40:15 +0300

    ipad-ipod support

--- index.php
+++ index.php
@@ -144,9 +144,7 @@
 
     <?php
     function isMobileDevice() {
-        return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo
-    |fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i"
-    , $_SERVER["HTTP_USER_AGENT"]);
+      return preg_match("/(android|webos|avantgo|iphone|ipad|ipod|blackberry|iemobile|bolt|boost|cricket|docomo|fone|hiptop|mini|opera mini|kitkat|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
     }
     if(isMobileDevice()){?>
       <script>
